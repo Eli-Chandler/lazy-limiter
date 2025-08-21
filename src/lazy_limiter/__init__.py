250e3012a0201e8ea73eea9430fb3f467d8d3b95
eli <eli.chandler@gmail.com>
2025-08-21 23:37:24 +1200

    normalise file names

--- src/lazy_limiter/__init__.py
+++ src/lazy_limiter/__init__.py
@@ -1,6 +1,6 @@
 from .token_bucket import TokenBucketRateLimiter
 from .keyed_rate_limiter import KeyedRateLimiter
-from .sliding_window_rate_limiter import SlidingWindowRateLimiter
+from .sliding_window import SlidingWindowRateLimiter
 __all__ = [
     "TokenBucketRateLimiter",
     "KeyedRateLimiter",
